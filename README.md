82843d19bd60a000eeefef39bda3f5533ea78dc6
Jake Bellacera <jake.bellacera@gmail.com>
2018-12-31 12:04:50 -0800

    Scaffold out the server

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ This is a project I created for learning React. It's a Slack clone that is built
 Each facet of the application is located in a subdirectory:
 
 * **client** - This houses the client application which is built in React
+* **server** - This houses the backend server which is powered by Node.js and Express
 
 ## VSCode Workspace
 
---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+// Put all API endpoints under '/api'
+app.get('/api/hello', (req, res) => {
+  res.json({body: 'Hello, world!'});
+});
+
+app.get('/api*', (req, res) => {
+  res.json({body: 'Endpoint not found.'});
+});
+
+// The "catch all" handler: for any request that doesn't match one above, send
+// back to the client's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+const HTTP_PORT = process.env.PORT || 5000;
+app.listen(HTTP_PORT);
+
+console.log(`Server listening on port ${HTTP_PORT}`);
