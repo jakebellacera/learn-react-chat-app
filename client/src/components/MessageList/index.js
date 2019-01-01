0a12007060110fa2764a8817c869b00d050b9c82
Jake Bellacera <jake.bellacera@gmail.com>
2018-12-31 16:41:49 -0800

    Show timestamps alongside messages

--- client/src/components/MessageList/index.js
+++ client/src/components/MessageList/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import './MessageList.scss';
 
 const MessageList = ({ messages }) => (
   <ul className="messagelist">
-    {messages.map(({ username, body }, i) => (
+    {messages.map(({ username, createdAt, body }, i) => (
       <li key={i} className="messagelist-item">
         <div className="messagelist-item-username">
           {username}
         </div>
+        <time
+          className="messagelist-item-date"
+          dateTime={moment(createdAt).format('YYYY-MM-DD HH:mm Z')}
+        >
+          {moment(createdAt).format('h:mm A')}
+        </time>
         <div className="messagelist-item-body">
           {body}
         </div>
